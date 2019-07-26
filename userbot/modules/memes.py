33a1f67cd0337a29baab76d84efa62f4feee9046
GoldenSkull0 <53174312+GoldenSkull0@users.noreply.github.com>
2019-07-26 08:37:33 +0530

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -736,7 +736,7 @@ async def hacking (hacked):
         if await hacked.get_reply_message():
             await hacked.edit(
                 "`Targeted Account Hacked successfully 😎......`\n"
-                "`Pay 6969$ to` @shivamkchoudhary `To Remove This Hack...`\n"
+                "`Pay 6969$ to`@GoldenSkull_xD `To Remove This Hack...`\n"
             )
 			  
 @register(outgoing=True, pattern="^.kill$")
